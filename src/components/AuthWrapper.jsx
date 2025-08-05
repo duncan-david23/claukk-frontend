1b5fbf9cc146d338cfb748d6ef910ca91535cb3c
D.A Duncan <d.aduncan@DAs-MacBook-Pro.local>
2025-08-05 16:18:26 +0000

    another loader import fixed

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { supabase } from "./supabase";
 import { Navigate } from "react-router";
-import Loader from "./loader";
 
 const AuthWrapper = ({ children }) => {
   const [authenticated, setAuthenticated] = useState(false);
